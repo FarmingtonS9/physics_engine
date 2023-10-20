d6017057b696a4e54456f68dcba5070e85c937f8
FarmingtonS9 <sframpton40@gmail.com>
2023-10-20 17:39:19 +1000

    Initial

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+//External libraries
+
+
+//Directory setup
+mod quantities;
+pub use crate::quantities::*;
+
+fn main() {
+    println!("Hello, world!");
+    println!("I am going to make a physics engine.");
+    println!("First things first, what physics method are focussing on?");
+    println!("Hamiltonian method would provide the most broad approach to real-world phenomena.")
+}
